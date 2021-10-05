22dccf2b40ae0508c2b9c5091a87a2b73ed4e651
unknown <sebastian.konicz@gmail.com>
2021-10-05 19:49:01 +0200

    changes for deploying project on heroku

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+import dash
+from dash import dcc
+from dash import html
+# import dash_core_components as dcc
+# import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
-app = Flask(__name__)
+app = dash.Dash(__name__)
+server = app.server
 
-@app.route('/')
-def render_the_map():
-    return render_template('vaccination_map.html')
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [5, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
