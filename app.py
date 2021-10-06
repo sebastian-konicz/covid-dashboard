0cc58b2a012d39a223300245178d4fe1e7cce34f
unknown <sebastian.konicz@gmail.com>
2021-10-06 21:16:27 +0200

    changes the method of saving files

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ server = app.server
 # project directory
 project_dir = str(Path(__file__).resolve().parents[0])
 
+print(project_dir)
+
 # # # # # DATA # # # # # #
 # loading dataframe
 # lopip freeze > requirements.txtading data
-data_path = r'\data\interim\vaccination_data\vaccinations_county_20211003.csv'
-data = pd.read_csv(project_dir + data_path)
+data_path = 'https://github.com/sebastian-konicz/covid-dashboard/raw/main/data/interim/vaccination_data/vaccinations_county_20211003.xlsx'
+data = pd.read_excel(data_path, engine='openpyxl')
 
 # restricting dataframe
 data = data[['teryt', 'powiat', '%_zaszczepieni']]
@@ -26,26 +28,27 @@ data = data[['teryt', 'powiat', '%_zaszczepieni']]
 data['teryt'] = data['teryt'].apply(lambda x: str(x).zfill(4))
 
 # loading geojson
+geo_path = 'https://github.com/sebastian-konicz/covid-dashboard/raw/main/data/interim/geo/geo_county.geojson'
 with open(project_dir + r'\data\interim\geo\geo_county.geojson') as file:
     geojson = gj.load(file)
-#
-# # get the maximum value to cap displayed values
-#     max_log = data['%_zaszczepieni'].max()
-#     min_val = data['%_zaszczepieni'].min()
-#     max_val = int(max_log) + 1
-#
-#     fig = px.choropleth_mapbox(data,
-#                                geojson=geojson,
-#                                featureidkey='properties.JPT_KOD_JE',
-#                                locations='teryt',
-#                                color='%_zaszczepieni',
-#                                color_continuous_scale=px.colors.diverging.RdBu,
-#                                range_color=(min_val, max_val),
-#                                mapbox_style="carto-positron",
-#                                zoom=5, center={"lat": 52, "lon": 19},
-#                                opacity=0.5,
-#                                )
-#     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+# get the maximum value to cap displayed values
+    max_log = data['%_zaszczepieni'].max()
+    min_val = data['%_zaszczepieni'].min()
+    max_val = int(max_log) + 1
+
+    fig = px.choropleth_mapbox(data,
+                               geojson=geojson,
+                               featureidkey='properties.JPT_KOD_JE',
+                               locations='teryt',
+                               color='%_zaszczepieni',
+                               color_continuous_scale=px.colors.diverging.RdBu,
+                               range_color=(min_val, max_val),
+                               mapbox_style="carto-positron",
+                               zoom=5, center={"lat": 52, "lon": 19},
+                               opacity=0.5,
+                               )
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 # # # # # # LAYOUT # # # # # #
 app.layout = html.Div([
