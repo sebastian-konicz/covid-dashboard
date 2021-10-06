9bc0a267d8815eb9f6225a3db07cbc7175f2c84d
unknown <sebastian.konicz@gmail.com>
2021-10-06 22:08:05 +0200

    changed file location to raw.github

--- app.py
+++ app.py
@@ -22,27 +22,27 @@ data = data[['teryt', 'powiat', '%_zaszczepieni']]
 data['teryt'] = data['teryt'].apply(lambda x: str(x).zfill(4))
 
 # loading geojson
-geo_path = 'https://github.com/sebastian-konicz/covid-dashboard/raw/main/data/interim/geo/geo_county.geojson'
+geo_path = 'https://raw.githubusercontent.com/sebastian-konicz/covid-dashboard/main/data/interim/geo/geo_county.geojson'
 with open(geo_path) as file:
     geojson = gj.load(file)
 
-# # get the maximum value to cap displayed values
-#     max_log = data['%_zaszczepieni'].max()
-#     min_val = data['%_zaszczepieni'].min()
-#     max_val = int(max_log) + 1
-#
-#     fig = px.choropleth_mapbox(data,
-#                                geojson=geojson,
-#                                featureidkey='properties.JPT_KOD_JE',
-#                                locations='teryt',
-#                                color='%_zaszczepieni',
-#                                color_continuous_scale=px.colors.diverging.RdBu,
-#                                range_color=(min_val, max_val),
-#                                mapbox_style="carto-positron",
-#                                zoom=5, center={"lat": 52, "lon": 19},
-#                                opacity=0.5,
-#                                )
-#     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+# get the maximum value to cap displayed values
+    max_log = data['%_zaszczepieni'].max()
+    min_val = data['%_zaszczepieni'].min()
+    max_val = int(max_log) + 1
+
+    fig = px.choropleth_mapbox(data,
+                               geojson=geojson,
+                               featureidkey='properties.JPT_KOD_JE',
+                               locations='teryt',
+                               color='%_zaszczepieni',
+                               color_continuous_scale=px.colors.diverging.RdBu,
+                               range_color=(min_val, max_val),
+                               mapbox_style="carto-positron",
+                               zoom=5, center={"lat": 52, "lon": 19},
+                               opacity=0.5,
+                               )
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 # # # # # # LAYOUT # # # # # #
 app.layout = html.Div([
@@ -50,7 +50,7 @@ app.layout = html.Div([
             style={'textAlign': 'center'}),
     dcc.Graph(
             id='example-map',
-            # figure=fig
+            figure=fig
     ),
 ])
 
