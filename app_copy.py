66ef273f64fc3b58d4758212d33c58ae67534829
unknown <sebastian.konicz@gmail.com>
2021-10-07 22:14:32 +0200

    changes in the venv packages

--- app_copy.py
+++ app_copy.py
@@ -4,10 +4,10 @@ import plotly.express as px
 import urllib.request
 
 import dash
-# from dash import dcc
-# from dash import html
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+# import dash_core_components as dcc
+# import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__)
@@ -57,65 +57,65 @@ fig_vac = px.choropleth_mapbox(data_vac,
 fig_vac.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
                       mapbox=dict(accesstoken='pk.eyJ1Ijoic2ViYXN0aWFua29uaWN6IiwiYSI6ImNrdWhjNGs0ZzE2bXAzMXJ0Y2RibmJ3OHMifQ.GRxIKFjuWBcrShcf6FCiJA'))
 
-# # # # # # # COVID CASES MAP # # # # # #
-# # get the maximum value to cap displayed values - vaccinations
-# max_log_cov = data_cov['zar_10k'].max()
-# min_val_cov = data_cov['zar_10k'].min()
-# max_val_cov = int(max_log_vac) + 1
-#
-# fig_cov = px.choropleth_mapbox(data_cov,
-#                            geojson=geojson,
-#                            featureidkey='properties.JPT_KOD_JE',
-#                            locations='teryt',
-#                            color='zar_10k',
-#                            color_continuous_scale=px.colors.diverging.RdBu_r,
-#                            range_color=(min_val_cov, max_val_cov),
-#                            mapbox_style="carto-darkmatter",
-#                            zoom=5, center={"lat": 52, "lon": 19},
-#                            opacity=0.5,
-#                            )
-# fig_cov.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
-#                       mapbox=dict(accesstoken='pk.eyJ1Ijoic2ViYXN0aWFua29uaWN6IiwiYSI6ImNrdWhjNGs0ZzE2bXAzMXJ0Y2RibmJ3OHMifQ.GRxIKFjuWBcrShcf6FCiJA'))
+# # # # # # COVID CASES MAP # # # # # #
+# get the maximum value to cap displayed values - vaccinations
+max_log_cov = data_cov['zar_10k'].max()
+min_val_cov = data_cov['zar_10k'].min()
+max_val_cov = int(max_log_vac) + 1
+
+fig_cov = px.choropleth_mapbox(data_cov,
+                           geojson=geojson,
+                           featureidkey='properties.JPT_KOD_JE',
+                           locations='teryt',
+                           color='zar_10k',
+                           color_continuous_scale=px.colors.diverging.RdBu_r,
+                           range_color=(min_val_cov, max_val_cov),
+                           mapbox_style="carto-positron",
+                           zoom=5, center={"lat": 52, "lon": 19},
+                           opacity=0.5,
+                           )
+fig_cov.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
+                      mapbox=dict(accesstoken='pk.eyJ1Ijoic2ViYXN0aWFua29uaWN6IiwiYSI6ImNrdWhjNGs0ZzE2bXAzMXJ0Y2RibmJ3OHMifQ.GRxIKFjuWBcrShcf6FCiJA'))
 
 # # # # # LAYOUT # # # # # #
-app.layout = html.Div([
-    html.H1('Mapa szczepień na COVID-19 w Polsce ',
-            style={'textAlign': 'center'}),
-    html.Div([
-        dcc.Graph(
-                id='vaccination_map',
-                figure=fig_vac
-        ),
-        # dcc.Graph(
-        #         id='covid_cases_map',
-        #         figure=fig_cov
-        # ),
-    ]),
-])
+# app.layout = html.Div([
+#     html.H1('Mapa szczepień na COVID-19 w Polsce ',
+#             style={'textAlign': 'center'}),
+#     html.Div([
+#         dcc.Graph(
+#                 id='vaccination_map',
+#                 figure=fig_vac
+#         ),
+#         # dcc.Graph(
+#         #         id='covid_cases_map',
+#         #         figure=fig_cov
+#         # ),
+#     ]),
+# ])
 
-# app.layout = dbc.Container([
-#             html.H1('Mapa szczepień na COVID-19 w Polsce ',
-#                     style={'textAlign': 'center'}),
-#
-#             dbc.Row(
-#                 [
-#                     dbc.Col(
-#                         dcc.Graph(
-#                             id='vaccination_map',
-#                             figure=fig_vac
-#                         )
-#                     ),
-#                     dbc.Col(
-#                         dcc.Graph(
-#                             id='covid_cases_map',
-#                             figure=fig_cov
-#                         )
-#                     ),
-#                 ],
-#             ),
-#         ],
-#         fluid=True,
-#     )
+app.layout = dbc.Container([
+            html.H1('Mapa szczepień na COVID-19 w Polsce ',
+                    style={'textAlign': 'center'}),
+
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Graph(
+                            id='vaccination_map',
+                            figure=fig_vac
+                        )
+                    ),
+                    dbc.Col(
+                        dcc.Graph(
+                            id='covid_cases_map',
+                            figure=fig_cov
+                        )
+                    ),
+                ],
+            ),
+        ],
+        fluid=True,
+    )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
