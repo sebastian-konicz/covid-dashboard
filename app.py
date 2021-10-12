9bc5e432b16b28797a2c697a5e618fd52d13a1e3
unknown <sebastian.konicz@gmail.com>
2021-10-12 18:41:05 +0200

    final changes

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ data_cov = pd.read_excel(data_cov_path, engine='openpyxl')
 data_cov = data_cov[data_cov['data'] == date]
 
 # restricting dataframe
-data_cov = data_cov[['teryt', 'powiat_miasto', 'zarazenia']]
+data_cov = data_cov[['teryt', 'powiat', 'zarazenia']]
 # reshaping teryt
 data_cov = data_cov[data_cov['teryt'] != 0]
 data_cov['teryt'] = data_cov['teryt'].apply(lambda x: str(x).zfill(4))
@@ -96,61 +96,45 @@ fig_cov = px.choropleth_mapbox(data_cov,
 fig_cov.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 # # # # # LAYOUT # # # # # #
-# app.layout = html.Div([
-#     html.H1('Mapa szczepień na COVID-19 w Polsce ',
-#             style={'textAlign': 'center'}),
-#     html.Div([
-#         dcc.Graph(
-#                 id='vaccination_map',
-#                 figure=fig_vac
-#         ),
-#         # dcc.Graph(
-#         #         id='covid_cases_map',
-#         #         figure=fig_cov
-#         # ),
-#     ]),
-# ])
-
 width = 6
 
 app.layout = dbc.Container([
-            html.H1('Mapa szczepień na COVID-19 w Polsce ',
-                    style={'textAlign': 'center'}),
-            dbc.Row([
-                dbc.Col(
-                    dcc.Markdown('''
-                        ##### Procent osób zaszczepionych przeciwko COVID19 w powiatach 
-                        **Źródło**: Oficjalne dane dot. zaszczepienia przeciwko COVID19 w gminach z portalu [Otwarte dane](https://dane.gov.pl/pl/dataset/2476,odsetek-osob-zaszczepionych-przeciwko-covid19-w-gm?fbclid=IwAR059OLAARQT9Umr02jVnfn9abacBD0ZF12fNyHH7m1hHXUswt-tufdMDsA)  
-                        **Uwagi**: Dane źródłowe zostały zagregowane z poziomu gmin do powiatów
-                        '''),
-                    width=width),
-                dbc.Col(
-                    dcc.Markdown('''
-                        ##### Procent osób głosujących na PiS w wyborach do parlamentu w 2019 roku w podziale na powiaty 
-                        **Źródło**: Dane Państwowej Komisji Wyborczej dot. wyborów do Sejmu i Senatu Rzeczypospolitej Polskiej 2019  [sejmsenat2019.pkw.gov.pl](https://sejmsenat2019.pkw.gov.pl/sejmsenat2019/data/csv/wyniki_gl_na_listy_po_powiatach_sejm_xlsx.zip)
-                         
-                        '''),
-                    width=width),
-            ]),
-            # dbc.Row(
-            #     [
-            #         dbc.Col([
-            #             dcc.Graph(
-            #                 id='vaccination_map',
-            #                 figure=fig_vac
-            #             )
-            #         ], width=width),
-            #         dbc.Col([
-            #             dcc.Graph(
-            #                 id='covid_cases_map',
-            #                 figure=fig_cov
-            #             )
-            #         ], width=width),
-            #     ],
-            # ),
-        ],
-        fluid=True,
-    )
+                html.H1('Mapa szczepień na COVID-19 w Polsce ',
+                        style={'textAlign': 'center'}),
+                dbc.Row([
+                    dbc.Col(
+                        dcc.Markdown('''
+                            ##### Procent osób zaszczepionych przeciwko COVID19 w powiatach 
+                            **Źródło**: Oficjalne dane dot. zaszczepienia przeciwko COVID19 w gminach z portalu [Otwarte dane](https://dane.gov.pl/pl/dataset/2476)  
+                            **Uwagi**: Dane źródłowe zostały zagregowane z poziomu gmin do powiatów
+                            '''),
+                        width=width),
+                    dbc.Col(
+                        dcc.Markdown('''
+                            ##### Liczba przypadków COVID-19 w podziale na powiaty 
+                            **Źródło**: Oficjalne dane dot. zaszczepienia przeciwko COVID19 w gminach z portalu  [Otwarte dane]((https://dane.gov.pl/en/dataset/2477))
+                             
+                            '''),
+                        width=width),
+                ]),
+                dbc.Row(
+                    [
+                        dbc.Col([
+                            dcc.Graph(
+                                id='vaccination_map',
+                                figure=fig_vac
+                            )
+                        ], width=width),
+                        dbc.Col([
+                            dcc.Graph(
+                                id='covid_cases_map',
+                                figure=fig_cov
+                            )
+                        ], width=width),
+                    ],
+                ),
+            ], fluid=True,
+        )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
